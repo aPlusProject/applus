98377f35dd0516f793b554d8ed5682178a68bf1c
vanessaduong <vanessa.duong93@yahoo.fr>
2016-05-26 23:20:50 +0200

    send json format

--- edu/client/socket/ChartTCPServer.java
+++ edu/client/socket/ChartTCPServer.java
@@ -15,7 +15,6 @@ import java.sql.ResultSet;
 
 import edu.aplus.db.ConnectionPool;
 
-
 public class ChartTCPServer {
 
 	// static ServerSocket variable
@@ -23,7 +22,7 @@ public class ChartTCPServer {
 	// socket server port on which it will listen
 	private static int port = 1234;
 	private static String receivedMsg;
-	private static String returnMsg;
+	private static String returnMsg = "";
 
 	public static void main(String args[]) throws Exception {
 
@@ -34,38 +33,50 @@ public class ChartTCPServer {
 		PrintWriter pw = new PrintWriter(fromClientSocket.getOutputStream(), true);
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(fromClientSocket.getInputStream()));
-		//receivedMsg = br.readLine();
-		
+		// receivedMsg = br.readLine();
+
 		System.out.println(receivedMsg);
 		ConnectionPool conn = new ConnectionPool();
-		Connection co= null;
+		Connection co = null;
 		conn.makeStack();
-		
+
 		co = conn.getConnection();
 		try {
 			PreparedStatement ps;
 			ResultSet rs;
 
-
-			String sql = "SELECT CLIENT_EMAIL FROM CLIENT";
+			String sql = "SELECT ID_LOAN, ASKED_AMOUNT,ASKED_DURATION,ASKED_RATE,ASKED_RATEINSURANCE,ASKED_DATE FROM LOAN";
 
 			ps = co.prepareStatement(sql);
-			
+
 			rs = ps.executeQuery();
 
-			while(rs.next()) {
+			while (rs.next()) {
 
-				System.out.println(rs.getString("CLIENT_EMAIL"));
-				returnMsg=returnMsg+";"+rs.getString("CLIENT_EMAIL");
+				// return JSON string
+				/*
+				 * {"loan_id":[ {"ASKED_AMOUNT":"10000", "ASKED_DURATION":"5",
+				 * "ASKED_RATE":"2", "ASKED_RATEINSURANCE":"2", "ASKED_DATE":
+				 * "2016-05-10 00:00:00.0"} ]}
+				 */
+				System.out.println(rs.getString("ASKED_AMOUNT"));
+				returnMsg = "{" + rs.getString("ID_LOAN") + ":[ {ASKED_AMOUNT:" + rs.getString("ASKED_AMOUNT")
+						+ ", ASKED_DURATION:" + rs.getString("ASKED_DURATION") + ",ASKED_RATE:"
+						+ rs.getString("ASKED_RATE") + ", ASKED_RATEINSURANCE:" + rs.getString("ASKED_RATEINSURANCE")
+						+ ",ASKED_DATE:" + rs.getString("ASKED_DATE") + "} ]}}";
 			}
 			rs.close();
 			ps.close();
 		} catch (Exception e) {
 			throw e;
-		}finally{
-			try{co.close();}catch(Exception e){;}
+		} finally {
+			try {
+				co.close();
+			} catch (Exception e) {
+				;
+			}
 		}
-		
+
 		co.close();
 		pw.println(returnMsg);
 
