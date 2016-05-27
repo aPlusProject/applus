39a3f238c17bf96bbac7c5236a356d826686fef7
Ji Na <jina2020@gmail.com>
2016-05-28 01:23:49 +0200

    New types of json parser added

--- edu/aplus/service/JsonParser_new.java
+++ edu/aplus/service/JsonParser_new.java
@@ -1,17 +1,15 @@
 package edu.aplus.service;
 
 import java.util.ArrayList;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
-
 import edu.aplus.model.Client;
 import edu.aplus.model.Employee;
+import edu.aplus.model.Loan;
+
 
 public class JsonParser_new {
-	
-	
 
 	public static Client JSonToObject(String json) {
 
@@ -22,12 +20,26 @@ public class JsonParser_new {
 		return client;
 	}
 	
+	public static Loan JSonToObjectLoan(String json) {
+
+		Gson gson = new GsonBuilder().create();
+
+		Loan loan = gson.fromJson(json, Loan.class);
+
+		return loan;
+	}
+
+	public static String ObjectToJSonLoan(Loan loan){
+		Gson gson = new GsonBuilder().create();
+		String json = gson.toJson(loan);
+		
+		return json;
+	}
+	
 	public static String ObjectToJSon(Client client){
 		Gson gson = new GsonBuilder().create();
 		String json = gson.toJson(client);
 		
 		return json;
 	}
-	
-
 }
