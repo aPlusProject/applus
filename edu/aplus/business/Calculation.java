30c72849b647d77fc9be9ee02b6f658a950746d5
vyach <sellathurai.vyach@yahoo.fr>
2016-05-28 04:25:24 +0200

    comment of elie work because it cause prbls

--- edu/aplus/business/Calculation.java
+++ edu/aplus/business/Calculation.java
@@ -7,88 +7,90 @@ import java.sql.Statement;
 
 
 public class Calculation {
+	
 	public static float totalinteret(int id_loan){
-		Connection2 b = new Connection2();
-	      Connection vCon = b.connection();
-	      String requete = "SELECT * FROM LOAN WHERE ID_LOAN="+id_loan;
-	      try {
-				Statement vSt = vCon.createStatement();
-				ResultSet vRs = vSt.executeQuery(requete);
-
-				
-				while(vRs.next()){
-					int Amount = vRs.getInt("ASKED_AMOUNT");
-					int Duration = vRs.getInt("ASKED_DURATION");
-					float rate = vRs.getFloat("ASKED_RATE");
-					rate = (float) rate/100;
-					float rate_month=(float)(Amount * rate)/12;
-					float rate_total = (float) rate_month * Duration;
-					return rate_total;
-					}
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		
-	     // (33500 * 8.12/100)/12
+//		Connection2 b = new Connection2();
+//	      Connection vCon = b.connection();
+//	      String requete = "SELECT * FROM LOAN WHERE ID_LOAN="+id_loan;
+//	      try {
+//				Statement vSt = vCon.createStatement();
+//				ResultSet vRs = vSt.executeQuery(requete);
+//
+//				
+//				while(vRs.next()){
+//					int Amount = vRs.getInt("ASKED_AMOUNT");
+//					int Duration = vRs.getInt("ASKED_DURATION");
+//					float rate = vRs.getFloat("ASKED_RATE");
+//					rate = (float) rate/100;
+//					float rate_month=(float)(Amount * rate)/12;
+//					float rate_total = (float) rate_month * Duration;
+//					return rate_total;
+//					}
+//			} catch (SQLException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//		
+//	     // (33500 * 8.12/100)/12
 		
 		return 0;
 	}
 	public static float totalinteretParMois(int id_loan){
-		Connection2 b = new Connection2();
-	      Connection vCon = b.connection();
-	      String requete = "SELECT * FROM LOAN WHERE ID_LOAN="+id_loan;
-	      try {
-				Statement vSt = vCon.createStatement();
-				ResultSet vRs = vSt.executeQuery(requete);
-
-				
-				while(vRs.next()){
-					int Amount = vRs.getInt("ASKED_AMOUNT");
-					float rate = vRs.getFloat("ASKED_RATE");
-					rate = (float) rate/100;
-					float rate_month=(float)(Amount * rate)/12;
-
-					return rate_month;
-					}
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+//		Connection2 b = new Connection2();
+//	      Connection vCon = b.connection();
+//	      String requete = "SELECT * FROM LOAN WHERE ID_LOAN="+id_loan;
+//	      try {
+//				Statement vSt = vCon.createStatement();
+//				ResultSet vRs = vSt.executeQuery(requete);
+//
+//				
+//				while(vRs.next()){
+//					int Amount = vRs.getInt("ASKED_AMOUNT");
+//					float rate = vRs.getFloat("ASKED_RATE");
+//					rate = (float) rate/100;
+//					float rate_month=(float)(Amount * rate)/12;
+//
+//					return rate_month;
+//					}
+//			} catch (SQLException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
 		
 	     // (33500 * 8.12/100)/12
 		
 		return 0;
 	}
 	public static float txdendettement(int id_loan){
-		Connection2 b = new Connection2();
-	      Connection vCon = b.connection();
-	      String requete = "SELECT * FROM LOAN WHERE ID_LOAN="+id_loan;
-	      try {
-				Statement vSt = vCon.createStatement();
-				ResultSet vRs = vSt.executeQuery(requete);
-				while(vRs.next()){
-					int id_client = vRs.getInt("ID_CLIENT");
-					float Amount = (float) vRs.getInt("ASKED_AMOUNT")/12;
-					String requete2 = "SELECT * FROM CLIENT WHERE ID_CLIENT="+id_client;
-					ResultSet vRs2 = vSt.executeQuery(requete2);
-			
-					while(vRs2.next()){
-						int salary = vRs2.getInt("SALARY");
-						int charge = vRs2.getInt("CHARGE");
-						float mensualite=totalinteretParMois(id_loan);
-						mensualite = mensualite + Amount;
-							
-						float tx = (float)(mensualite + charge)/salary;
-						return tx;
-					}
-					
-					}
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
 		
+//		Connection2 b = new Connection2();
+//	      Connection vCon = b.connection();
+//	      String requete = "SELECT * FROM LOAN WHERE ID_LOAN="+id_loan;
+//	      try {
+//				Statement vSt = vCon.createStatement();
+//				ResultSet vRs = vSt.executeQuery(requete);
+//				while(vRs.next()){
+//					int id_client = vRs.getInt("ID_CLIENT");
+//					float Amount = (float) vRs.getInt("ASKED_AMOUNT")/12;
+//					String requete2 = "SELECT * FROM CLIENT WHERE ID_CLIENT="+id_client;
+//					ResultSet vRs2 = vSt.executeQuery(requete2);
+//			
+//					while(vRs2.next()){
+//						int salary = vRs2.getInt("SALARY");
+//						int charge = vRs2.getInt("CHARGE");
+//						float mensualite=totalinteretParMois(id_loan);
+//						mensualite = mensualite + Amount;
+//							
+//						float tx = (float)(mensualite + charge)/salary;
+//						return tx;
+//					}
+//					
+//					}
+//			} catch (SQLException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//		
 
 		
 		return 0;
--- edu/aplus/gui/Fenetre.java
+++ edu/aplus/gui/Fenetre.java
@@ -80,80 +80,80 @@ public class Fenetre extends JFrame {
 	  			
   JTable table = new JTable(model); 
 
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setLocationRelativeTo(null);
-	    container.setBackground(Color.white);
-	    container.setLayout(new BorderLayout());
-	    combo.setPreferredSize(new Dimension(100, 20));
-	    combo.setName("1");
-	    
-	    //REQUETE FORM
-	    Connection2 a = new Connection2();
-	    Connection vCon = a.connection();
-	      
-		try {
-			Statement vSt = vCon.createStatement();
-			ResultSet vRs = vSt.executeQuery("SELECT * FROM LOAN WHERE ID_CLIENT=1");
-			while(vRs.next()){
-				int vNom = vRs.getInt(1);
-				combo.addItem(vNom);
-				combo2.addItem(vNom);
-				//System.out.println("Nom="+vNom);
-				}
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	    combo.addItemListener(new ItemState());
-	    combo2.setPreferredSize(new Dimension(100, 20));
-	    combo2.addItemListener(new ItemState2());
-	    Button.addActionListener(new ItemAction());
-	    
-	    JPanel top = new JPanel();
-	    JPanel panelTableau = new JPanel();
-	    panelTableau.setLayout(new BorderLayout()) ;
-	    panelTableau.add(new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.SOUTH);
-
-	    top.add(label);
-
-	    top.add(combo);
-	    top.add(label2);
-	    top.add(combo2);
-	    top.add(Button);
-	   //bottom.add(table);
-	    container.add(top, BorderLayout.PAGE_START);
-	    container.add(panelTableau, BorderLayout.CENTER);
-	    
-		//  getContentPane().add(table.getTableHeader(), BorderLayout.SOUTH);
-	  //  getContentPane().add(table, BorderLayout.SOUTH);
-	//    container.add(bottom, BorderLayout.SOUTH);
-	   
-	    this.setContentPane(container2);
-	    this.setContentPane(container);
-	    
-	    this.setVisible(true);    
+//	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	    this.setLocationRelativeTo(null);
+//	    container.setBackground(Color.white);
+//	    container.setLayout(new BorderLayout());
+//	    combo.setPreferredSize(new Dimension(100, 20));
+//	    combo.setName("1");
+//	    
+//	    //REQUETE FORM
+//	    Connection2 a = new Connection2();
+//	    Connection vCon = a.connection();
+//	      
+//		try {
+//			Statement vSt = vCon.createStatement();
+//			ResultSet vRs = vSt.executeQuery("SELECT * FROM LOAN WHERE ID_CLIENT=1");
+//			while(vRs.next()){
+//				int vNom = vRs.getInt(1);
+//				combo.addItem(vNom);
+//				combo2.addItem(vNom);
+//				//System.out.println("Nom="+vNom);
+//				}
+//		} catch (SQLException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//	    combo.addItemListener(new ItemState());
+//	    combo2.setPreferredSize(new Dimension(100, 20));
+//	    combo2.addItemListener(new ItemState2());
+//	    Button.addActionListener(new ItemAction());
+//	    
+//	    JPanel top = new JPanel();
+//	    JPanel panelTableau = new JPanel();
+//	    panelTableau.setLayout(new BorderLayout()) ;
+//	    panelTableau.add(new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.SOUTH);
+//
+//	    top.add(label);
+//
+//	    top.add(combo);
+//	    top.add(label2);
+//	    top.add(combo2);
+//	    top.add(Button);
+//	   //bottom.add(table);
+//	    container.add(top, BorderLayout.PAGE_START);
+//	    container.add(panelTableau, BorderLayout.CENTER);
+//	    
+//		//  getContentPane().add(table.getTableHeader(), BorderLayout.SOUTH);
+//	  //  getContentPane().add(table, BorderLayout.SOUTH);
+//	//    container.add(bottom, BorderLayout.SOUTH);
+//	   
+//	    this.setContentPane(container2);
+//	    this.setContentPane(container);
+//	    
+//	    this.setVisible(true);    
 	    
 	  }
  
 	  
 	  
 	  public static int indebt(){
-		  Connection2 a = new Connection2();
-		  Connection vCon = a.connection();
-	      
-			try {
-				Statement vSt = vCon.createStatement();
-				ResultSet vRs = vSt.executeQuery("SELECT * FROM CLIENT");
-				while(vRs.next()){
-					String vNom = vRs.getString(1);
-					String vPrenom = vRs.getString(2);
-					System.out.println("Nom="+vNom+"Prenom="+vPrenom);
-					}
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
+//		  Connection2 a = new Connection2();
+//		  Connection vCon = a.connection();
+//	      
+//			try {
+//				Statement vSt = vCon.createStatement();
+//				ResultSet vRs = vSt.executeQuery("SELECT * FROM CLIENT");
+//				while(vRs.next()){
+//					String vNom = vRs.getString(1);
+//					String vPrenom = vRs.getString(2);
+//					System.out.println("Nom="+vNom+"Prenom="+vPrenom);
+//					}
+//			} catch (SQLException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}
+//			
 		  
 		  return 0;
 	  }
@@ -188,86 +188,86 @@ public class Fenetre extends JFrame {
 		    public void actionPerformed(ActionEvent e) {
 		     // System.out.println("ActionListener : action sur " + combo.getSelectedItem());
 		    //  System.out.println("ActionListener : action sur " + combo2.getSelectedItem());
-		      String requete1 = "SELECT * FROM LOAN WHERE ID_LOAN="+combo.getSelectedItem();
-		      String requete2 = "SELECT * FROM LOAN WHERE ID_LOAN="+combo2.getSelectedItem();
-		      int id_loan1 = (int) combo.getSelectedItem();
-		      int id_loan2 = (int) combo2.getSelectedItem();
-
-		      
-		      getInfo info = new getInfo();
-		      
-		      Calculation calcul = new Calculation();
-		      float total1 = calcul.totalinteret(id_loan1);
-		      float total2 = calcul.totalinteret(id_loan2);
-		      float total3 = (float) total1-total2;
-		      float mois1 = calcul.totalinteretParMois(id_loan1);
-		      float mois2 = calcul.totalinteretParMois(id_loan2);
-		      float mois3 = (float) mois1-mois2;
-		      
-		      
-		      System.out.println(total1);
-		      System.out.println(total2);
-		     // Connection2 a = new Connection2();
-		    //  Connection vCon = a.connection();
-		    /*  try {
-					Statement vSt = vCon.createStatement();
-					ResultSet vRs = vSt.executeQuery(requete1);
-					ResultSet vRs2 = vSt.executeQuery(requete2);
-					int vNom = vRs.getInt(8);
-					int vNom2 = vRs2.getInt(8);
-					System.out.println("Nom="+vNom+vNom2);
-					
-				/*	while(vRs.next() && vRs2.next()){
-						int vNom = vRs.getInt(8);
-						int vNom2 = vRs2.getInt(8);
-						combo.addItem(vNom);
-						combo2.addItem(vNom);
-						System.out.println("Nom="+vNom+vNom2);
-						}
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}*/
-
-
-		   //   model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
-		   //   model.fireTableDataChanged();
-		      while (i<8){
-		    	  model.removeRow(0);
-		    	  i = i + 1;
-		      }
-		      
-		     
-		      
-	
-		   
-		      
-			/*try {
-				Statement vSt = vCon.createStatement();
-				ResultSet vRs = vSt.executeQuery("SELECT * FROM CLIENT");
-				while(vRs.next()){
-					/*String vNom = vRs.getString(1);
-					String vPrenom = vRs.getString(2);
-					System.out.println("Nom="+vNom+"Prenom="+vPrenom);
-					}
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}*/
-		      
-		   
-		      
-		      model.addRow(new Object[]{"Montant demandé", info.asked_amount(id_loan1)+" €", info.asked_amount(id_loan2)+" €", info.asked_amount(id_loan1)-info.asked_amount(id_loan2)+" €"});
-		      model.addRow(new Object[]{"Nombre de mois", info.asked_duration(id_loan1), info.asked_duration(id_loan2), info.asked_duration(id_loan1)-info.asked_duration(id_loan2)});
-		      model.addRow(new Object[]{"Décision", info.decision(id_loan1), info.decision(id_loan2), 0});
-		      model.addRow(new Object[]{"Total des interets/mois", mois1+" €", mois2+" €", mois3+" €"});
-		      model.addRow(new Object[]{"Total des interets", total1+" €", total2+" €", total3+" €"});
-		      model.addRow(new Object[]{"Taux d'endettement", calcul.txdendettement(id_loan1), calcul.txdendettement(id_loan2), 0});
-		      model.addRow(new Object[]{"Niveau d'endettement", "Column 2", "Column 3", "Column 3"});
-		      model.addRow(new Object[]{"Poids dans la mensualité", "Column 2", "Column 3", "Column 3"});
-		      i=0;
-		      //compare((String)combo.getSelectedItem(),(String)combo2.getSelectedItem());
-		      
+//		      String requete1 = "SELECT * FROM LOAN WHERE ID_LOAN="+combo.getSelectedItem();
+//		      String requete2 = "SELECT * FROM LOAN WHERE ID_LOAN="+combo2.getSelectedItem();
+//		      int id_loan1 = (int) combo.getSelectedItem();
+//		      int id_loan2 = (int) combo2.getSelectedItem();
+//
+//		      
+////		      getInfo info = new getInfo();
+//		      
+//		      Calculation calcul = new Calculation();
+//		      float total1 = calcul.totalinteret(id_loan1);
+//		      float total2 = calcul.totalinteret(id_loan2);
+//		      float total3 = (float) total1-total2;
+//		      float mois1 = calcul.totalinteretParMois(id_loan1);
+//		      float mois2 = calcul.totalinteretParMois(id_loan2);
+//		      float mois3 = (float) mois1-mois2;
+//		      
+//		      
+//		      System.out.println(total1);
+//		      System.out.println(total2);
+//		     // Connection2 a = new Connection2();
+//		    //  Connection vCon = a.connection();
+//		    /*  try {
+//					Statement vSt = vCon.createStatement();
+//					ResultSet vRs = vSt.executeQuery(requete1);
+//					ResultSet vRs2 = vSt.executeQuery(requete2);
+//					int vNom = vRs.getInt(8);
+//					int vNom2 = vRs2.getInt(8);
+//					System.out.println("Nom="+vNom+vNom2);
+//					
+//				/*	while(vRs.next() && vRs2.next()){
+//						int vNom = vRs.getInt(8);
+//						int vNom2 = vRs2.getInt(8);
+//						combo.addItem(vNom);
+//						combo2.addItem(vNom);
+//						System.out.println("Nom="+vNom+vNom2);
+//						}
+//				} catch (SQLException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}*/
+//
+//
+//		   //   model.addRow(new Object[]{"Column 1", "Column 2", "Column 3"});
+//		   //   model.fireTableDataChanged();
+//		      while (i<8){
+//		    	  model.removeRow(0);
+//		    	  i = i + 1;
+//		      }
+//		      
+//		     
+//		      
+//	
+//		   
+//		      
+//			/*try {
+//				Statement vSt = vCon.createStatement();
+//				ResultSet vRs = vSt.executeQuery("SELECT * FROM CLIENT");
+//				while(vRs.next()){
+//					/*String vNom = vRs.getString(1);
+//					String vPrenom = vRs.getString(2);
+//					System.out.println("Nom="+vNom+"Prenom="+vPrenom);
+//					}
+//			} catch (SQLException e1) {
+//				// TODO Auto-generated catch block
+//				e1.printStackTrace();
+//			}*/
+//		      
+//		   
+//		      
+//		      model.addRow(new Object[]{"Montant demandé", info.asked_amount(id_loan1)+" €", info.asked_amount(id_loan2)+" €", info.asked_amount(id_loan1)-info.asked_amount(id_loan2)+" €"});
+//		      model.addRow(new Object[]{"Nombre de mois", info.asked_duration(id_loan1), info.asked_duration(id_loan2), info.asked_duration(id_loan1)-info.asked_duration(id_loan2)});
+//		      model.addRow(new Object[]{"Décision", info.decision(id_loan1), info.decision(id_loan2), 0});
+//		      model.addRow(new Object[]{"Total des interets/mois", mois1+" €", mois2+" €", mois3+" €"});
+//		      model.addRow(new Object[]{"Total des interets", total1+" €", total2+" €", total3+" €"});
+//		      model.addRow(new Object[]{"Taux d'endettement", calcul.txdendettement(id_loan1), calcul.txdendettement(id_loan2), 0});
+//		      model.addRow(new Object[]{"Niveau d'endettement", "Column 2", "Column 3", "Column 3"});
+//		      model.addRow(new Object[]{"Poids dans la mensualité", "Column 2", "Column 3", "Column 3"});
+//		      i=0;
+//		      //compare((String)combo.getSelectedItem(),(String)combo2.getSelectedItem());
+//		      
 
 		      
 		      
